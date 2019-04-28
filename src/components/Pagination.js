1862eaf55fe35ccac22dde81640d7d0bfa20e2ff
Ishrak <ishrak.imam@gmail.com>
2019-04-29 01:06:02 +0600

    pagination component customization

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -6,32 +6,78 @@ import styles from './Pagination.css'
 class Pagination extends Component {
   constructor (props) {
     super(props)
+    this.calculatePages = this.calculatePages.bind(this)
     this.goToPage = this.goToPage.bind(this)
   }
 
   goToPage (page) {
     return () => {
-      if (this.props.currentPage !== page) {
+      if (this.props.currentPage !== page && !(page < 1) && !(page > this.props.pages.length)) {
         this.props.goToPage(page)
       }
     }
   }
 
+  calculatePages (pages, page) {
+    const currentPage = Number(page)
+
+    if (pages.length <= 4) return pages
+    const pagesChunk = []
+
+    if (currentPage === pages.length) {
+      pagesChunk.push('dot')
+      for (let i = currentPage - 2; i <= currentPage; i++) {
+        pagesChunk.push(String(i))
+      }
+      return pagesChunk
+    }
+
+    if (currentPage === 1) {
+      for (let i = 1; i <= currentPage + 2; i++) {
+        pagesChunk.push(String(i))
+      }
+      pagesChunk.push('dot')
+      return pagesChunk
+    }
+
+    if (pages.length > 4 && Math.ceil(pages.length / 2) > currentPage) {
+      for (let i = currentPage - 1; i <= currentPage + 1; i++) {
+        pagesChunk.push(String(i))
+      }
+      pagesChunk.push('dot')
+    }
+    if (pages.length > 4 && Math.ceil(pages.length / 2) <= currentPage) {
+      pagesChunk.push('dot')
+      for (let i = currentPage - 1; i <= currentPage + 1; i++) {
+        pagesChunk.push(String(i))
+      }
+    }
+    return pagesChunk
+  }
+
   render () {
-    const { pages, currentPage } = this.props
+    let { pages, currentPage } = this.props
+
+    const chunkedPages = this.calculatePages(pages, currentPage)
+
     return (
       <div className={styles.pagination}>
-        <button onClick={this.goToPage(pages[0])} className={`${styles.anchor}`}>&laquo;</button>
-        {pages.map(page => (
-          <button
-            onClick={this.goToPage(page)}
-            key={page}
-            className={classnames(styles.anchor, {
-              'active': currentPage === page
-            })}
-          >{page}</button>
-        ))}
-        <button onClick={this.goToPage(pages[pages.length - 1])} className={`${styles.anchor}`}>&raquo;</button>
+        <button onClick={this.goToPage('1')} className={`${styles.anchor}`}>&laquo;</button>
+        <button onClick={this.goToPage(String(Number(currentPage) - 1))} className={`${styles.anchor}`}>&lsaquo;</button>
+        {chunkedPages.map(page => {
+          if (page === 'dot') return <span key={page}>. . .</span>
+          return (
+            <button
+              onClick={this.goToPage(page)}
+              key={page}
+              className={classnames(styles.anchor, {
+                'active': currentPage === page
+              })}
+            >{page}</button>
+          )
+        })}
+        <button onClick={this.goToPage(String(Number(currentPage) + 1))} className={`${styles.anchor}`}>&rsaquo;</button>
+        <button onClick={this.goToPage(String(pages.length))} className={`${styles.anchor}`}>&raquo;</button>
       </div>
     )
   }
